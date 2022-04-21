278b5cf13bdf3e9bfe1e7a94ebd972767bbb273f
Aaron Enyetu <aaronenyetu@gmail.com>
2022-04-21 10:55:35 -0700

    My Information

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 // import functions
 
 // grab DOM elements
-
 // set event listeners 
-    // get user input
+// get user input
     // use user input to update state 
+document.getElementById ('clickButton').addEventListener('click', () => {document.getElementById('myInfo').value = 'Aaron';});
+document.getElementById ('clickButton').addEventListener('click', () => {document.getElementById('pronouns').value = 'he/him';});
+document.getElementById ('clickButton').addEventListener('click', () => {document.getElementById('city').value = 'Austin, Texas';});
+  
     // update DOM to reflect the new state
